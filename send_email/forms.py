470c742479eb8f139d91ed9c2b4913168c24347f
Rok Jenčič <rok.jencic@gmail.com>
2017-07-23 23:01:09 +0200

    Learning to add Bootstrap classes to elements

--- send_email/forms.py
+++ send_email/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(label='Your Email', required=True)
+    email = forms.EmailField(label='Your Email', required=True)
     subject = forms.CharField(label='Subject', required=True)
-    message = forms.CharField(widget=forms.Textarea, label='Message', required=True)
+    message = forms.CharField(widget=forms.Textarea, label='Message', help_text='Enter your message', required=True)
