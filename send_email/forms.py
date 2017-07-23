4ba555ad365c001bb5d9b6a7cdd5b22f87cbc1e4
Rok Jenčič <rok.jencic@gmail.com>
2017-07-23 23:16:42 +0200

    More successful testing

--- send_email/forms.py
+++ send_email/forms.py
@@ -4,4 +4,5 @@ from django import forms
 class ContactForm(forms.Form):
     email = forms.EmailField(label='Your Email', required=True)
     subject = forms.CharField(label='Subject', required=True)
-    message = forms.CharField(widget=forms.Textarea, label='Message', help_text='Enter your message', required=True)
+    message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Enter your inquiry'}), label='Message',
+                              help_text='Enter your message', required=True)
