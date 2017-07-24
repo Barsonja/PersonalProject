ddef98f5fc3f223823bf1c0a1a56733aa4ae3efb
Rok Jenčič <rok.jencic@gmail.com>
2017-07-24 04:10:03 +0200

    Small changes

--- send_email/forms.py
+++ send_email/forms.py
@@ -2,7 +2,8 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    email = forms.EmailField(label='Your Email', widget=forms.TextInput(attrs={'placeholder': 'Your Email'}), required=True)
+    email = forms.EmailField(label='Your Email', widget=forms.TextInput(attrs={'placeholder': 'Enter Your Email'}),
+                             required=True)
     subject = forms.CharField(label='Subject', required=True)
     message = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Enter your inquiry'}), label='Message',
                               help_text='Enter your message', required=True)
