8c4da61672263305ebc8c3f38671a58d4177e15f
Rok Jenčič <rok.jencic@gmail.com>
2017-07-30 12:19:37 +0200

    Added new models

--- prenovi/models.py
+++ prenovi/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
-from datetime import datetime
+from datetime import datetime, timedelta
+import uuid
 
 # Create your models here.
 
@@ -14,3 +15,34 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Contractor(models.Model):
+    name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    work = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+
+    def get_absolute_url(self):
+        return reverse('contractor-detail-view', args=[str(self.id)])
+
+    def __str__(self):
+        return self.last_name + ' ' + self.name
+
+def get_end_date():
+    return datetime.today() + timedelta(days=30)
+
+
+class WorkInstance(models.Model):
+    client_name=models.CharField(max_length=100)
+    title = models.CharField(max_length=100, help_text="Name of the project")
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text="Unique ID for this particular workinstance  across whole Database")
+    contractor = models.ForeignKey('Contractor',null=True, on_delete=models.SET_NULL)
+    start_date = models.DateTimeField(default=datetime.today())
+    end_date = models.DateTimeField(default=get_end_date)
+    summary = models.TextField(max_length=1000, blank=True, null=True)
+
+    def __str__(self):
+        return self.title + ', ' + self.client_name
